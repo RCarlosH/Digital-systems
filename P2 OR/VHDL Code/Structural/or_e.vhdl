ee101f70bdf812ae817d82f7df9ff3adffe18ddc
rcarlosh <up210281@alumnos.upa.edu.mx>
2023-06-11 12:57:32 -0600

    or_e

--- P2 OR/VHDL Code/Structural/or_e.vhdl
+++ P2 OR/VHDL Code/Structural/or_e.vhdl
@@ -1,16 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity and_e is
+entity or_e is
 	port(--ent:in std_logic_vector(1 downto 0); -- 2bit port
 	A,B:in std_logic;
 	F:out std_logic);
 end entity;
 
-architecture arq of and_e is
+architecture arq of or_e is
 begin
 	process (A,B)
 	begin
-	F <= A and B;
+	F <= A or B;
 end process;
 end architecture;
